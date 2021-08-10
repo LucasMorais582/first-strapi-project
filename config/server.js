e9a743791d52d76c3e2cd068fb81e9da8239326a
Lucas Morais <lucasmorais582@gmail.com>
2021-08-10 14:31:21 -0300

    Creating first project with strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1f0f9408ac966b24b8aabd59881a885e'),
+    },
+  },
+});
